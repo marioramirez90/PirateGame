8d9b3e69bb066566e2effc743cb965260cf2e898
Mario Ramirez <mario-ramirez@hotmail.de>
2025-07-28 12:29:27 +0200

    add: cheackAttack

--- pirategame2d/skripte/character.gd
+++ pirategame2d/skripte/character.gd
@@ -11,43 +11,71 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var hassSword = false
 
+var isAttacking = false
+var toggleAttack = false
+
+func checkAttack():
+	if Input.is_action_just_pressed("ui_attack"):
+		isAttacking = true 
+		$AttackArea/CollisionShape2D.disabled = false
+		animSprite.play("attack1") 
+		if(animSprite.flip_h):
+			$AttackArea/CollisionShape2D.position.x += -1
+			toggleAttack = true
+
+
+
+
 func _physics_process(delta):
 	
 	var swordExtra = "_nos"
 	
 	if(hassSword):
 		swordExtra = "_hs"
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		print(velocity.y)
-		
-		if(velocity.y > 0):
-			animSprite.play("fall" + swordExtra)
-		else:
-			animSprite.play("jump" + swordExtra)
-	else:
-		if(velocity.x == 0):
-			animSprite.play("idle" + swordExtra)
-		else:
-			animSprite.play("run" + swordExtra)
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if(isAttacking == false):
+		# Add the gravity.
+		if not is_on_floor():
+			velocity.y += gravity * delta
+			print(velocity.y)
+			
+			if(velocity.y > 0):
+				animSprite.play("fall" + swordExtra)
+			else:
+				animSprite.play("jump" + swordExtra)
+		else:
+			if(velocity.x == 0):
+				animSprite.play("idle" + swordExtra)
+			else:
+				animSprite.play("run" + swordExtra)
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+		# Handle jump.
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction = Input.get_axis("ui_left", "ui_right")
+		if direction:
+			velocity.x = direction * SPEED
 		# links rechts animation
-		if(velocity.x < 0):
-			animSprite.flip_h = true
+			if(velocity.x < 0):
+				animSprite.flip_h = true
+			else:
+				animSprite.flip_h = false
 		else:
-			animSprite.flip_h = false
-		
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			
+		if(hassSword):
+			checkAttack()
 
 	move_and_slide()
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	isAttacking = false
+	$AttackArea/CollisionShape2D.disabled = true
+	if(toggleAttack):
+		$AttackArea/CollisionShape2D.position.x += -1
+		toggleAttack =false
+		

ab988c78fb195077ebe34202622c8415683b0de3
Mario Ramirez <mario-ramirez@hotmail.de>
2025-07-28 21:57:57 +0200

    add:GUI

---
+++ pirategame2d/skripte/main_hud.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	$VBoxContainer/HBoxContainer/RedDiamodsValue.text = str(Global.redDiamond)
+	$VBoxContainer/HBoxContainer2/GoldCoinValue.text = str(Global.goldCoins)

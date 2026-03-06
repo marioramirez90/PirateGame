44a768bbf27a7566457cef1037af887d4b90bd35
marioramirez90 <mario-ramirez@hotmail.de>
2026-03-06 09:25:49 +0100

    Increase camera zoom level for mobile devices

--- skripte/character.gd
+++ skripte/character.gd
@@ -23,7 +23,7 @@ func _ready() -> void:
 func _setup_camera_zoom() -> void:
 	# Nur auf mobilen Geräten stärker zoomen
 	if DisplayServer.is_touchscreen_available():
-		camera.zoom = Vector2(2.0, 2.0)  # Stärker zoomen auf mobil
+		camera.zoom = Vector2(3.0, 3.0)  # Stärker zoomen auf mobil
 	
 func playerHit():
 	reciveDamage = true
